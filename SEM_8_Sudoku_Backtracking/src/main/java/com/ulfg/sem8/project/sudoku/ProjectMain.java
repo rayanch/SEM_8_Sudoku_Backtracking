f04d39e8275917b2c04218bb910364ebc7f1815c
rayanch <36073317+rayanch@users.noreply.github.com>
2022-06-11 16:16:29 +0300

    Update File Name

--- SEM_8_Sudoku_Backtracking/src/main/java/com/ulfg/sem8/project/sudoku/ProjectMain.java
+++ SEM_8_Sudoku_Backtracking/src/main/java/com/ulfg/sem8/project/sudoku/ProjectMain.java
@@ -1,14 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.ulfg.sem8.project.sudoku;
 
-/**
- *
- * @author user
- */
 public class ProjectMain {
 
     public static void main(String[] args) {
