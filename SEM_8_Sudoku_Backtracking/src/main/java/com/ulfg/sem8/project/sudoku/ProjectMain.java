03ea2ccfe6a7d067f3d791eb9959ddaa40727e24
rayanch <36073317+rayanch@users.noreply.github.com>
2022-06-14 19:54:48 +0300

    Added Logger class

--- SEM_8_Sudoku_Backtracking/src/main/java/com/ulfg/sem8/project/sudoku/ProjectMain.java
+++ SEM_8_Sudoku_Backtracking/src/main/java/com/ulfg/sem8/project/sudoku/ProjectMain.java
@@ -1,11 +1,13 @@
 package com.ulfg.sem8.project.sudoku;
 
+import com.ulfg.sem8.project.sudoku.UI.MainFrame;
+import com.ulfg.sem8.project.util.Logger;
+
 public class ProjectMain {
 
+    public static Logger logger;
+    
     public static void main(String[] args) {
-        /*MainFrame mf = new MainFrame("Test");
-        mf.setVisible(true);*/
-        
         /*
         Solution hard:
             3 1 6  5 7 8  4 9 2 
@@ -31,17 +33,6 @@ public class ProjectMain {
                         "0, 0, 0, 0, 0, 0, 0, 7, 4," +
                         "0, 0, 5, 2, 0, 6, 3, 0, 0";
         
-        String hard_half_solved =   
-                        "3, 1, 6, 5, 7, 8, 4, 9, 2," + 
-                        "5, 2, 9, 1, 3, 4, 7, 6, 8," +
-                        "4, 8, 7, 6, 2, 9, 5, 3, 1," +
-                        "2, 6, 3, 4, 1, 5, 9, 8, 7," +
-                        "9, 7, 4, 8, 6, 3, 1, 2, 5," +
-                        "8, 5, 1, 7, 9, 2, 6, 4, 3," +
-                        "1, 3, 8, 9, 4, 7, 2, 5, 6," +
-                        "6, 9, 2, 3, 5, 1, 0, 7, 4," +
-                        "7, 0, 5, 2, 0, 6, 3, 0, 0";
-        
         String easy =   "8, 7, 6, 9, 0, 0, 0, 0, 0," + 
                         "0, 1, 0, 0, 0, 6, 0, 0, 0," +
                         "0, 4, 0, 3, 0, 5, 8, 0, 0," +
@@ -52,11 +43,13 @@ public class ProjectMain {
                         "0, 0, 4, 6, 9, 0, 1, 7, 3," +
                         "0, 0, 0, 0, 0, 1, 0, 0, 4";
         
+        logger = new Logger();
+        logger.mute(); // Comment this to show backtracking steps
+        
         SudokuGrid sg = new SudokuGrid(9);
-        sg.fromString(easy);
+        sg.fromString(hard);
         SudokuSolver solver = new SequentialSudokuSolver(sg);
         solver.solveGrid();
         System.out.println(solver.getGrid().textFormatGrid());
-        //System.out.println(sg.textFormatGrid());
     }
 }
